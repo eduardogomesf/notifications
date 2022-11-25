83ccb968739499c5e9dec30d286fc4b1b2d4b57b
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-25 20:45:41 -0300

    feat: add default theme with colors and font sizes

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,19 @@
+export const defaultTheme = {
+    colors: {
+        red: '#F65552',
+
+        blue: '#0A327B',
+        'very-dark-gray-blue': '#1C202B',
+        'dark-grey-blue': '#5E6778',
+        'grey-blue': '#939CAD',
+        'light-grey-blue': '#E5EFFA',
+        'very-light-grey-blue': '#DDE7EE',
+
+        snow: '#F7FAFD',
+        white: '#FFFFFF'
+    },
+    fonts: {
+        md: '1rem',
+        lg: '1.5rem'
+    }
+} as const
